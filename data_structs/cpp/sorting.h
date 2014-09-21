9a014b8d1a1c170dbab04d9cafadb562725b2cd9
rmpadsala <rakesh.padsala@gmail.com>
2014-09-21 12:15:24 -0500

    init sorting

---
+++ data_structs/cpp/sorting.h
@@ -0,0 +1,23 @@
+//
+//  sorting.h
+//  AlgorithmsAndDataStructs
+//
+//  Created by Rakesh Patel on 9/21/14.
+//  Copyright (c) 2014 Rakesh Patel. All rights reserved.
+//
+
+#ifndef AlgorithmsAndDataStructs_sorting_h
+#define AlgorithmsAndDataStructs_sorting_h
+
+namespace algo_ns {
+    
+    template <typename CollectionT = std::vector<int>, typename SortingStrategyT>
+    class SortingImpl {
+    public:
+        static void sort(const CollectionT& collection, const SortingStrategyT& strategy) {
+            
+        }
+    };
+}
+
+#endif
