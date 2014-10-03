94980123e3d639a59310bd1ef0cbd714bdcde0b7
rmpadsala <rakesh.padsala@gmail.com>
2014-10-02 23:23:29 -0500

    cpp init merge_sort

---
+++ data_structs/cpp/AlgorithmsAndDataStructs/algorithms/sorting/merge_sort.cpp
@@ -0,0 +1,9 @@
+//
+//  merge_sort.cpp
+//  AlgorithmsAndDataStructs
+//
+//  Created by Rakesh Patel on 10/2/14.
+//  Copyright (c) 2014 Rakesh Patel. All rights reserved.
+//
+
+#include "merge_sort.h"
---
+++ data_structs/cpp/AlgorithmsAndDataStructs/algorithms/sorting/merge_sort.h
@@ -0,0 +1,24 @@
+//
+//  merge_sort.h
+//  AlgorithmsAndDataStructs
+//
+//  Created by Rakesh Patel on 10/2/14.
+//  Copyright (c) 2014 Rakesh Patel. All rights reserved.
+//
+
+#ifndef __AlgorithmsAndDataStructs__merge_sort__
+#define __AlgorithmsAndDataStructs__merge_sort__
+
+#include <stdio.h>
+#include <vector>
+
+namespace algo_ns {
+    class MergeSort {
+    public:
+        static void sort(std::vector<int>& collection) {}
+        
+    private:
+    };
+}
+
+#endif /* defined(__AlgorithmsAndDataStructs__merge_sort__) */
