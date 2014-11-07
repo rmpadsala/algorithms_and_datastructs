fb2a767093c958b5817aa0a3293d71a0dc7176ac
rmpadsala <rakesh.padsala@gmail.com>
2014-11-07 11:34:18 -0600

    directed/undirected graphs init continued...

--- data_structs/ruby/graphs/undirected/graph.rb
+++ data_structs/ruby/graphs/undirected/graph.rb
@@ -11,17 +11,32 @@ module Graphs
       end
 
       def add_edge(v, w)
+        validate(v)
+        validate(w)
+
         @adj[v] << w
         @adj[w] << v
         @edges += 1
       end
 
       def adj(v)
+        validate(v)
+
         @adj[v]
       end
 
+      def print
+        @adj.each_with_index do |v, i|
+          v.each do |e|
+            p "#{i}->#{e}"
+          end
+        end
+      end
       # duplicate graph
-
+      private
+        def validate(v)
+          raise "out of bound expception" if v < 0 || v >= vertices
+        end
     end
   end
 end
