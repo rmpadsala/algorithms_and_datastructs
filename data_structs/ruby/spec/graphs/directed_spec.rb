391ca48f97925d935ec0fe798b28e4df51e9381c
rmpadsala <rakesh.padsala@gmail.com>
2014-11-08 23:42:22 -0600

    graph specs

--- data_structs/ruby/spec/graphs/directed_spec.rb
+++ data_structs/ruby/spec/graphs/directed_spec.rb
@@ -1,13 +1,34 @@
 require "spec_helper"
 
-describe "undirect_graph" do
+describe "directed_graph" do
+  before :each do
+    @directed_g = Graphs::Directed::Graph.new(5)
+  end
+
   describe ".instance_methods" do
-    before { @undirected_g = Graphs::Undirected::Graph.new(5) }
     it "should respond to instance methods" do
-      expect(@undirected_g).to respond_to(:vertices)
-      expect(@undirected_g).to respond_to(:edges)
-      expect(@undirected_g).to respond_to(:add_edge)
-      expect(@undirected_g).to respond_to(:vertices)
+      expect(@directed_g).to respond_to(:vertices)
+      expect(@directed_g).to respond_to(:edges)
+      expect(@directed_g).to respond_to(:add_edge)
+      expect(@directed_g).to respond_to(:vertices)
+    end
+  end
+
+  describe ".add_edge, .adj" do
+    it "should add edge from v to w" do
+      @directed_g.add_edge(1,2)
+      @directed_g.add_edge(2,3)
+
+      expect(@directed_g.adj(1)).to eq([2])
+      expect(@directed_g.adj(2)).to eq([3])
+    end
+  end
+
+  describe ".edges" do
+    it "should set correct number of edges" do
+      @directed_g.add_edge(1,2)
+      @directed_g.add_edge(2,3)
+      expect(@directed_g.edges).to eq(2)
     end
   end
 end
--- data_structs/ruby/spec/graphs/undirected_spec.rb
+++ data_structs/ruby/spec/graphs/undirected_spec.rb
@@ -1,13 +1,35 @@
 require "spec_helper"
 
-describe "undirect_graph" do
+describe "undirected_graph" do
+  before :each do
+    @undirected_g = Graphs::Undirected::Graph.new(5)
+  end
+
   describe ".instance_methods" do
-    before { @undirected_g = Graphs::Undirected::Graph.new(5) }
     it "should respond to instance methods" do
       expect(@undirected_g).to respond_to(:vertices)
       expect(@undirected_g).to respond_to(:edges)
       expect(@undirected_g).to respond_to(:add_edge)
       expect(@undirected_g).to respond_to(:vertices)
+    end
+  end
+
+  describe ".add_edge, .adj" do
+    it "should add edge from v to w" do
+      @undirected_g.add_edge(1,2)
+      @undirected_g.add_edge(2,3)
+
+      expect(@undirected_g.adj(1)).to eq([2])
+      expect(@undirected_g.adj(2)).to eq([1,3])
+      expect(@undirected_g.adj(3)).to eq([2])
+    end
+  end
+
+  describe ".edges" do
+    it "should set correct number of edges" do
+      @undirected_g.add_edge(1,2)
+      @undirected_g.add_edge(2,3)
+      expect(@undirected_g.edges).to eq(2)
     end
   end
 end
