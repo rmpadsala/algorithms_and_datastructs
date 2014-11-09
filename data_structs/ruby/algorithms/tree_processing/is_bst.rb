7c12abbf2b0212196d21d946580be9350f9d4125
rmpadsala <rakesh.padsala@gmail.com>
2014-11-09 16:16:56 -0600

    is_bst impl

--- data_structs/ruby/algorithms/tree_processing/is_bst.rb
+++ data_structs/ruby/algorithms/tree_processing/is_bst.rb
@@ -1,6 +1,25 @@
 module Algorithms
   module TreeProcessing
-    def self.bst?(tree)
+    class IsBst
+      def self.bst?(tree)
+        root = tree.root
+
+        validate?(root, nil, nil)
+      end
+
+      def self.validate?(node, min, max)
+        return true if node.nil?
+
+        p "key: #{node.key}, min: #{min}, max: #{max}"
+
+        return false if min != nil && node.key <= min
+        return false if max != nil && node.key >= max
+
+        return validate?(node.left, min, node.key) &&
+               validate?(node.right, node.key, max)
+      end
+
+      private_class_method :validate?
     end
   end
 end
